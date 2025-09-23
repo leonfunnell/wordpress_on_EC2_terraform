b32a34a9eea2cd6ec336d6843bdf871e9782409c
Leon Funnell <leon_funnell@hotmail.com>
2025-09-23 13:39:13 +0100

    Futureproof: Always select the latest Ubuntu LTS AMI (xx.04) using jq filter in deploy_wordpress.sh.

--- deploy_wordpress.sh
+++ deploy_wordpress.sh
@@ -2,8 +2,15 @@
 
 source variables.sh
 
-# Find the latest Ubuntu LTS AMI (e.g., 24.04, 22.04, etc.)
-AMI=$(aws ec2 describe-images --owners 099720109477 --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*' 'Name=state,Values=available' --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId' --output text --region $AWS_REGION --profile $AWS_PROFILE)
+# Find the latest Ubuntu LTS AMI (futureproof, always picks latest xx.04)
+AMI=$(aws ec2 describe-images --owners 099720109477 \
+  --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*' 'Name=state,Values=available' \
+  --region $AWS_REGION --profile $AWS_PROFILE \
+  --query "Images[?contains(Name, '.04')].{Name:Name,ImageId:ImageId,CreationDate:CreationDate}" \
+  --output json | \
+  jq -r '.[] | select(.Name | test("ubuntu-[a-z]+-[0-9]{2}\\.04-")) | [.CreationDate, .ImageId] | @tsv' | \
+  sort | tail -n1 | cut -f2
+)
 
 case "$1" in
 --destroy)
