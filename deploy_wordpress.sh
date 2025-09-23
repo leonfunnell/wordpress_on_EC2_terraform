fb125d687d5caeafbb697d2c8ce6ff6a0273dd58
Leon Funnell <leon_funnell@hotmail.com>
2025-09-23 12:53:14 +0100

    Always fetch the latest Ubuntu LTS AMI for deployment.

--- deploy_wordpress.sh
+++ deploy_wordpress.sh
@@ -2,11 +2,8 @@
 
 source variables.sh
 
-
-# Find the latest Ubuntu 22.04 AMI
-AMI=$(aws ec2 describe-images --owners 099720109477 --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*' 'Name=state,Values=available' --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId' --output text --region $AWS_REGION --profile $AWS_PROFILE)
-
-
+# Find the latest Ubuntu LTS AMI (e.g., 24.04, 22.04, etc.)
+AMI=$(aws ec2 describe-images --owners 099720109477 --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*' 'Name=state,Values=available' --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId' --output text --region $AWS_REGION --profile $AWS_PROFILE)
 
 case "$1" in
 --destroy)
