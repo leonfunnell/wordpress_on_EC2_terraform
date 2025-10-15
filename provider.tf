66792beb3537e277271263a4cf63cd2dfd36e97a
Leon Funnell <leon_funnell@hotmail.com>
2025-10-15 13:12:59 +0100

    feat(cloudfront): add us-east-1 AWS provider alias for ACM/CloudFront

--- provider.tf
+++ provider.tf
@@ -2,3 +2,10 @@ provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
 }
+
+# Additional provider for ACM certificates used by CloudFront (must be in us-east-1)
+provider "aws" {
+  alias   = "us_east_1"
+  profile = var.aws_profile
+  region  = "us-east-1"
+}
