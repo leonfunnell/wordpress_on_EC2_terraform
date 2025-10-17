03e3b338b9e85d666b33031bd348a8b9768af333
Leon Funnell <leon_funnell@hotmail.com>
2025-10-17 11:07:46 +0100

    feat(route53): point domain to EIP or ALB (alias) depending on enable_alb; remove CloudFront logic

--- route53.tf
+++ route53.tf
@@ -1,15 +1,26 @@
-# Route53 records
-# If CloudFront is enabled with a certificate, point domain to CF; otherwise point to EIP
+# Route53 records for site A/ALIAS
 
-resource "aws_route53_record" "wordpress_site" {
-  count = var.domain_name != "" && var.route53_zone_id != "" && !(var.enable_cloudfront && var.use_existing_certificate && var.existing_certificate_arn != "") ? 1 : 0
-
-  zone_id = var.route53_zone_id
-  name    = var.domain_name
-  type    = "A"
-  ttl     = 300
-
-  records = [aws_eip.wordpress_eip.public_ip]
-
+# A record to EIP if ALB disabled
+resource "aws_route53_record" "site_a_eip" {
+  count           = var.domain_name != "" && var.route53_zone_id != "" && !var.enable_alb ? 1 : 0
+  zone_id         = var.route53_zone_id
+  name            = var.domain_name
+  type            = "A"
+  ttl             = 300
+  records         = [aws_eip.wordpress_eip.public_ip]
   allow_overwrite = var.overwrite_dns_records
+}
+
+# ALIAS to ALB if enabled
+resource "aws_route53_record" "site_alias_alb" {
+  count           = var.domain_name != "" && var.route53_zone_id != "" && var.enable_alb ? 1 : 0
+  zone_id         = var.route53_zone_id
+  name            = var.domain_name
+  type            = "A"
+  allow_overwrite = var.overwrite_dns_records
+  alias {
+    name                   = aws_lb.wp[0].dns_name
+    zone_id                = aws_lb.wp[0].zone_id
+    evaluate_target_health = false
+  }
 }
